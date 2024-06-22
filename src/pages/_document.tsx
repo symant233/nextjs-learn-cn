8dc96b6c59b733a44c06a7af6e131b28536676dc
symant233 <symant233@gmail.com>
2024-06-22 22:39:39 +0800

    feat: remove qrcode, add image dark mode filter, remove footer

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,11 @@
 import { Head, Html, Main, NextScript } from "next/document";
-const inlineScript = `
-  var _hmt = _hmt || [];
-  (function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?d1a3a3a4d626d62cd30aa268728916d5";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-  })();
-`;
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script dangerouslySetInnerHTML={{ __html: inlineScript }} />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="/nextjs-learn-cn/favicon.ico" />
       </Head>
       <body>
         <Main />
--- theme.config.jsx
+++ theme.config.jsx
@@ -1,12 +1,7 @@
 import { useRouter } from "next/router";
-import Image from 'next/image';
-import codingMayImage from 'public/coding-may.jpg';
-import qrcodeImage from 'public/qrcode_for_coding_world.avif';
 
 const metaTags = (
   <>
-    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-    <link rel="icon" type="image/x-icon" href="/nextjs-learn-cn/favicon.ico" />
     <meta
       name="description"
       content="基于 App Router 架构的免费学习教程，通过构建全栈 Web 应用程序，让您更好的了解 Next.js 主要功能"
@@ -17,32 +12,6 @@ const metaTags = (
     <meta property="og:image" content="/chapter0-dashboard.avif" />
   </>
 );
-
-const ExtraContent = () => {
-  return <>
-    <div class="flex flex-col items-center">
-      <Image
-        src={codingMayImage}
-        className="mb-4 rounded-md shadow-lg"
-        alt="扫码备注 “nextjs” 加入 Next.js 中文技术交流群"
-        width={200}
-        height={200}
-      />
-      <p class="nx-text-xs font-bold text-center">扫码备注 “nextjs”<br/>加入 Next.js 中文技术交流群</p>
-
-      <div class="h-5"></div>
-
-      <Image
-        src={qrcodeImage}
-        className="mb-4 rounded-md shadow-lg"
-        alt="扫码备注 “nextjs” 加入 Next.js 中文技术交流群"
-        width={200}
-        height={200}
-      />
-      <p class="nx-text-xs font-bold text-center">关注公众号 “编程界”<br/>接收 Next.js 最新资讯</p>
-    </div>
-  </>
-}
 
 const config = {
   head: metaTags,
@@ -60,7 +29,6 @@ const config = {
   },
   toc: {
     backToTop: true,
-    extraContent: <ExtraContent />,
   },
   feedback: { content: null },
   useNextSeoProps() {
@@ -76,18 +44,7 @@ const config = {
     };
   },
   gitTimestamp: null,
-  footer: {
-    text: (
-      <span>
-        <a
-          href="https://github.com/qufei1993/nextjs-learn-cn/blob/main/LICENSE"
-          target="_blank"
-        >
-          MIT License - Copyright (c) 2023
-        </a>
-      </span>
-    ),
-  },
+  footer: false,
 };
 
 export default config;
